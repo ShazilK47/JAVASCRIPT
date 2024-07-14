94589075ecc2c0c4317aa906f28d87b3d9bef5c6
ShazilK47 <shazil.akn@gmail.com>
2024-07-14 18:57:30 +0500

    helping file

--- masterJS/01_basic/02_dataTypes.js
+++ masterJS/01_basic/02_dataTypes.js
@@ -1,1 +1,5 @@
 "use strict"; // it will treat all JS Code as Newer Verson
+
+// alert(3+3);        //as we are using node.js .. so this command will not work as it work in browser.
+
+//imp note: your code should be readible.

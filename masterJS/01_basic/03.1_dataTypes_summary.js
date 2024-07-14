46ecadf4995ab0d8cd4b805de34ebd94b03c0fe3
ShazilK47 <shazil.akn@gmail.com>
2024-07-14 21:00:15 +0500

    datatype summary

---
+++ masterJS/01_basic/03.1_dataTypes_summary.js
@@ -0,0 +1,3 @@
+// Data memory mein kis tarah se store hota hai and kis tarah se hum apne data ko access krte hen is bunyad per datatypes ko to category mein devide kia gaya hai
+// 1. Premitive datatypes
+// 2. Non premitive datatypes

a3d8cd6605cd4335794e60e454fa0416c4701cb7
ShazilK47 <shazil.akn@gmail.com>
2024-07-14 19:11:09 +0500

    understanding DataTypes

--- masterJS/01_basic/02_dataTypes.js
+++ masterJS/01_basic/02_dataTypes.js
@@ -3,3 +3,33 @@
 // alert(3+3);        //as we are using node.js .. so this command will not work as it work in browser.
 
 //imp note: your code should be readible.
+
+let name = "Shazil"; //string
+let age = 20; //number
+let isLoggedIn = true; //Boolean
+
+//# Datatypes in JavaScript
+
+// ## primitative DataTypes
+//number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true or false
+// null => standalone value          ---> an object
+// undefined => value is not assigned
+// symbol => unique value
+
+// ## Non-Primitative DataTypes
+// function => function name(){} => function is a datatype in JS
+// array => [] => array is a datatype in JS
+// object => {} => object is a datatype in JS
+// date => new Date() => date is a datatype in JS
+// regExp => new RegExp() => regExp is a datatype in JS
+// map => new Map() => map is a datatype in JS
+
+// object => {} => array, function, object, date, regExp, map, set,
+
+//note: we can use "typeof" key for checking the type of any variable
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined

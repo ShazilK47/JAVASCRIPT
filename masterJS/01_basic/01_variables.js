5555ff77bca23f71269bb937727274df67bf11f0
ShazilK47 <shazil.akn@gmail.com>
2024-07-17 11:56:31 +0500

    updated

--- masterJS/01_basic/01_variables.js
+++ masterJS/01_basic/01_variables.js
@@ -1,22 +1,22 @@
 //Key words to declare variable in JS
-//1.const 
-//2.let 
+//1.const
+//2.let
 //3.var
 
-const accountId = 833434     //unchangable
-let accountEmail = "shazil.akn@gmail.com"
-var accountPassword = "5555"
-accountCity = "karachi"   //In Js Variables also be declare like that but don't do it as it is a bad practice
-let accountState;       //undefined
+const accountId = 833434; //unchangable
+let accountEmail = "shazil.akn@gmail.com";
+var accountPassword = "5555";
+accountCity = "karachi"; //In Js Variables also be declare like that but don't do it as it is a bad practice
+let accountState; //undefined
 
 // accountId = 2; //not allowed
 
-accountEmail = "sk@gmail.com"
-accountPassword = "4332"
-accountCity = "Islamabad"  //it's possible in js to declare variable like that but don't do it
+accountEmail = "sk@gmail.com";
+accountPassword = "4332";
+accountCity = "Islamabad"; //it's possible in js to declare variable like that but don't do it
 
 console.log(accountId);
-console.table([accountId,accountEmail,accountPassword,accountCity]);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
 
 //     {
 //             --> this is scope
@@ -24,5 +24,5 @@ console.table([accountId,accountEmail,accountPassword,accountCity]);
 
 /*
 Prefer not to use var
-because of issue ne block scope and functional scope
+because of issue in block scope and functional scope
 */

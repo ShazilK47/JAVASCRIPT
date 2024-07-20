e756d6a7fc3ee68db7250f03c95014ac2896ab99
ShazilK47 <shazil.akn@gmail.com>
2024-07-20 11:19:54 +0500

    mini hoisting

--- masterJS/03_basic/02_Scope.js
+++ masterJS/03_basic/02_Scope.js
@@ -37,3 +37,32 @@ console.log(c); //40
 
 //Note that the values in a and b does'nt effected by the values declared inside the conditional scope
 //but the value c is changed because var is a global scope
+
+//nested scope
+
+function one() {
+  let username = "Shazil";
+
+  function two() {
+    let website = "MrAi.com";
+    console.log(username); //=> accessing variable from the outer function
+  }
+  //   console.log(website);   => Note that website is not accessible outside the nested function
+  two();
+}
+one();
+
+// *********************************** Interesting ***********************************************
+//hoisting
+
+//let's check the difference between two ways to declare function
+
+console.log(addOne(3, 5)); //this one is okay if we call the funciton before initilization
+function addOne(a, b) {
+  return a + b;
+}
+
+console.log(addTwo(10, 20)); //but this gives an error as it is saved in a variable
+let addTwo = function (a, b) {
+  return a + b;
+};

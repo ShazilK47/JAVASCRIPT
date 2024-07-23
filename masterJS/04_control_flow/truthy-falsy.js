b2e3e2d19498fd2dcab6fd548f3918d5d1ff30dc
ShazilK47 <shazil.akn@gmail.com>
2024-07-23 11:48:42 +0500

    truthy-falsy values

--- masterJS/04_control_flow/truthy-falsy.js
+++ masterJS/04_control_flow/truthy-falsy.js
@@ -18,7 +18,7 @@
 // []
 // {}
 // -1
-// functio() {}
+// function() {}
 
 //if we want to check if a value is truthy or falsy we can use the
 //Boolean() function
@@ -30,3 +30,48 @@ if (userEmail) {
 } else {
   console.log("Please provide your email address");
 }
+
+//to check empty arrays. we should check like this
+const users = ["Shazil", "Abdullah", "Imran"];
+if (users.length === 0) {
+  console.log(`Array is empty`);
+}
+
+//for empty object
+const emptyObj = {};
+if (Object.keys(emptyObj) == 0) {
+  console.log(`Object is empty`);
+}
+
+//some other practices
+// false == 0          => true
+// false == ""          => true
+// 0 == ""            => true
+
+//Logical Operators
+//And => &&
+//Or  => ||
+//Not => !
+
+//Nullish Coalescing Operator: ??  => null, undefined
+
+let val1;
+val1 = 5 ?? 10;
+console.log(val1); //5;
+
+val1 = null ?? 10;
+console.log(val1); //10;
+
+val1 = undefined ?? 15;
+console.log(val1);
+
+val1 = null ?? 10 ?? 20;
+console.log(val1); //10;
+
+//var2 = undefined ?? 10; => undefined
+
+// Ternary Operator
+// Syntax:  condition ? true : false;
+
+let price = 100;
+price > 80 ? console.log(`Greater then 80`) : console.log(`Less then 80`);
